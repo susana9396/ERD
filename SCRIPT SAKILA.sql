db40b66fe47d45fcc4947d073cf85afa5bd1993d
susana9396 <60800793+susana9396@users.noreply.github.com>
2020-07-06 21:39:45 -0400

    Update SCRIPT SAKILA.sql

--- SCRIPT SAKILA.sql
+++ SCRIPT SAKILA.sql
@@ -26,10 +26,8 @@ select * FROM film_category;
 select * FROM category;
 SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name
 FROM film
-JOIN film_category
-ON film.film_id=film_category.film_id
-JOIN category
-ON category.category_id=film_category.category_id
+   JOIN film_category ON film.film_id=film_category.film_id
+   JOIN category ON category.category_id=film_category.category_id
 WHERE category.name='Comedy'
 ;
 
@@ -39,10 +37,8 @@ WHERE category.name='Comedy'
 
 SELECT actor.first_name, actor.last_name, actor.actor_id,  film.title, film.description, film.release_year
 FROM actor
-JOIN film_actor
-ON actor.actor_id=film_actor.actor_id
-JOIN film
-ON film.film_id=film_actor.film_id
+  JOIN film_actor ON actor.actor_id=film_actor.actor_id
+  JOIN film ON film.film_id=film_actor.film_id
 WHERE actor.actor_id=5
 ;
 
@@ -51,8 +47,7 @@ WHERE actor.actor_id=5
 
 SELECT customer.first_name ,  customer.last_name ,  customer.email, address.address, address.city_id , customer.store_id
 FROM customer
-JOIN address
-ON  customer.address_id=address.address_id
+  JOIN address ON  customer.address_id=address.address_id
 WHERE customer.store_id=1 AND address.city_id  IN (1,42,312,459)
 ;
 
@@ -62,7 +57,7 @@ WHERE customer.store_id=1 AND address.city_id  IN (1,42,312,459)
 
 SELECT film.title, film.description, film.release_year, film.rating, film.special_features
 FROM film
-WHERE film.special_features LIKE '%Behind the Scenes%' AND film.rating='G'
+  WHERE film.special_features LIKE '%Behind the Scenes%' AND film.rating='G'
 ;
 
 -- 6. ¿Qué consulta harías para obtener todos los actores que se unieron en el film_id = 369? Su consulta 
@@ -70,10 +65,8 @@ WHERE film.special_features LIKE '%Behind the Scenes%' AND film.rating='G'
 
 SELECT  film.film_id, film.title ,  actor.actor_id,  actor.first_name, actor.last_name
 FROM film
-JOIN film_actor
-ON film.film_id=film_actor.film_id
-JOIN actor
-ON actor.actor_id=film_actor.actor_id
+  JOIN film_actor ON film.film_id=film_actor.film_id
+  JOIN actor ON actor.actor_id=film_actor.actor_id
 WHERE film.film_id=369
 ;
 
@@ -84,10 +77,8 @@ WHERE film.film_id=369
 
 SELECT film.title, film.description, film.rating, film.special_features, category.name As categoria 
 FROM film
-JOIN film_category
-ON film.film_id=film_category.film_id
-JOIN category
-ON category.category_id=film_category.category_id
+  JOIN film_category ON film.film_id=film_category.film_id
+  JOIN category ON category.category_id=film_category.category_id
 WHERE film.rental_rate=2.99 AND category.name='Drama'
 ;
 
